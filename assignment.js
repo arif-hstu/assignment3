38e7c0fa38a93f376fa80eb96654be74fd4900ac
arif-hstu <arif.dvm.hstu@gmail.com>
2021-01-22 16:41:02 +0600

    FINAL REVISION COMPLETE

--- assignment.js
+++ assignment.js
@@ -1,7 +1,7 @@
 // https://github.com/arif-hstu/assignment3
 
 function kilometerToMeter(kilometer) {
-    //checking if the input is a valid number
+    // checking if the input is a valid number
     if (isNaN(kilometer) !== true && kilometer >= 0) {
         // defining the variables
         var meter = 0;
@@ -11,15 +11,15 @@ function kilometerToMeter(kilometer) {
 
         return meter;
 
+        // if invalid input found
     } else {
-        //if invalid input found
         var errorText = 'Please input a valid number.';
         return errorText;
     }
 }
 
 function budgetCalculator(clock, phone, laptop) {
-    //checking if the inputs are valid numbers
+    // checking if the inputs are valid numbers
     if (isNaN(clock) !== true && clock >= 0 && isNaN(phone) !== true && phone >= 0 && isNaN(laptop) !== true && laptop >= 0) {
         // defining the variables
         var clockPrice = 0;
@@ -41,15 +41,15 @@ function budgetCalculator(clock, phone, laptop) {
 
         return totalPrice;
 
+    // if invalid input found
     } else {
-        //if invalid input found
         var errorText = 'Please input valid number(s).';
         return errorText;
     }
 }
 
 function hotelCost(days) {
-    //checking if the inputs are valid number
+    // checking if the input is a valid number
     if (isNaN(days) !== true && days >= 0) {
         // defining the variables
         var firstCost = 0;
@@ -57,28 +57,31 @@ function hotelCost(days) {
         var thirdCost = 0;
         var totalCost = 0;
 
+        // if the days are under or equal 10
         if (days <= 10) {
-            // if the days are under or equal 10
             totalCost = days * 100;
 
+        // if the days are under or equal 20
         } else if (days <= 20) {
-            // if the days are under or equal 20
+
             firstCost = 10 * 100;
             secondCost = (days - 10) * 80;
+
             totalCost = firstCost + secondCost;
 
+        // if the days are over 20
         } else {
-            // if the days are over 20
             firstCost = 10 * 100;
             secondCost = 10 * 80;
             thirdCost = (days - 20) * 50;
+
             totalCost = firstCost + secondCost + thirdCost;
         }
 
         return totalCost;
 
+    // if invalid input found
     } else {
-        //if invalid input found
         var errorText = 'Please input valid day(s).';
         return errorText;
     }
@@ -90,12 +93,12 @@ function megaFriend(friendsArray) {
     var result = '';
 
     if (Array.isArray(friendsArray)) {
+        // checking if the array is undefined or empty 
         if (friendsArray === undefined || friendsArray.length == 0) {
-            // checking if the array is undefined or empty 
             result = 'Your Array is empty. Please input some Names.';
 
+        // checking if the array has only one input
         } else if (friendsArray.length == 1) {
-            // checking if the array has only one input.
             result = 'You have used only one input. Please input two or more Names to compare.'
 
         } else {
@@ -124,8 +127,3 @@ function megaFriend(friendsArray) {
 
     return result;
 }
-
-console.log(kilometerToMeter(40.5));
-console.log(budgetCalculator(10, 8, 2));
-console.log(hotelCost(23));
-console.log(megaFriend(['arif', 'afroza', 'mona']));
